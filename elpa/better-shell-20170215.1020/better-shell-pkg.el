289668ecb1eddc88a4fdfbc150e4f2876f013766
Michael <mbrockman1@gmail.com>
2017-02-27 10:30:32 -0500

    Update

---
+++ elpa/better-shell-20170215.1020/better-shell-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "better-shell" "20170215.1020" "Better shell management" '((emacs "24.4")) :url "https://github.com/killdash9/better-shell" :keywords '("convenience"))
